ebd7a41f2113b2ff50ef4307d953a4a8cf652434
Muhammad Zubair <md.zubair143598@gmail.com>
2025-09-26 03:00:37 +0500

    created about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import Different from '@/components/about/different/Different'
+import Hero from '@/components/about/hero/Hero'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Hero />
+      <Different />
+    </div>
+  )
+}
+
+export default page
